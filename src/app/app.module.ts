8c6c2073db10a146365da93c07251de48d173ba7
Ildoctore77 <alexmar46@gmail.com>
2021-10-14 13:18:01 +0200

    version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent} from './cabecera/cabecera.component';
+import { ArticuloComponent } from './articulos/articulo.component';
+import { PrincipalComponent} from './principal/principal.component';
+
+import {ArticuloService} from './articulos/articulo.service';
+
+import {RouterModule,Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '',redirectTo:'/home',pathMatch:'full'},
+  {path: 'articulos', component:ArticuloComponent},
+  {path: 'home', component:PrincipalComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    ArticuloComponent,
+    PrincipalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ArticuloService],
+  bootstrap: [AppComponent,CabeceraComponent,ArticuloComponent,PrincipalComponent]
 })
 export class AppModule { }
